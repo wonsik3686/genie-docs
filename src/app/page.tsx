52c6cc16c47f99cbf25b950d8d6c92354f759141
wonsik <70436955+wonsik3686@users.noreply.github.com>
2025-03-20 16:40:34 +0900

    feat(three): 3D 모델 및 텍스처 추가, 메인 씬 구성
    - @react-three/drei 및 @react-three/fiber 패키지 추가
    - 새로운 3D 모델 'lamp.gltf' 및 관련 텍스처 파일 추가
    - 메인 씬 구성 요소로 'MainScene' 컴포넌트 생성 및 3D 텍스트와 조명 설정
    - 페이지 레이아웃 수정하여 메인 씬을 포함하도록 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
+import MainScene from '@/components/three/MainScene';
+
 export default function Home() {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
-        <div className="flex flex-col gap-2">
+    <div className="flex min-h-screen flex-col items-center justify-center gap-16 p-8 pb-20 sm:p-20">
+      <MainScene />
+      <main className="flex flex-col items-center justify-center gap-8 sm:items-start">
+        <div className="flex flex-col items-center justify-center gap-2">
           <h1 className="text-4xl-medium">Genie Docs</h1>
         </div>
       </main>
-      <footer className="row-start-3 flex flex-wrap items-center justify-center gap-6">
+      <footer className="flex flex-wrap items-center justify-center gap-6">
         <p>Genie Docs</p>
       </footer>
     </div>
